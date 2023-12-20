4d7025541496adf09ee40687e95425c61c950535
elvaraelvara <107627367+elvaraelvara@users.noreply.github.com>
2023-12-20 18:09:42 +0700

    Add files via upload

---
+++ ricther/dns.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "nameserver 192.168.122.1" >/etc/resolv.conf
+
+apt update
+apt install bind9 dnsutils -y
+
+# Backup existing file
+cp /etc/bind/named.conf.options /etc/bind/named.conf.options/bak
+
+# Configuration data
+echo 'options {
+    listen-on-v6 { none; };
+    directory "/var/cache/bind";
+
+    # Forwarders
+    forwarders {
+        192.168.122.1;
+    };
+
+    # If there is no answer from the forwarders, dont attempt to resolve recursively
+    forward only;
+
+    dnssec-validation no;
+
+    auth-nxdomain no;    # conform to RFC1035
+    allow-query { any; };
+};' >/etc/bind/named.conf.options
+
+service bind9 restart
+service bind9 status
